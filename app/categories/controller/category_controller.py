d391fbe879afcb12b93856692cd3c24d57ee7b79
Remy349 <santoraga15@gmail.com>
2024-01-19 19:49:29 -0600

    feat: Added alembic to handle database migrations.

--- app/categories/controller/category_controller.py
+++ app/categories/controller/category_controller.py
@@ -1,2 +1,10 @@
+from sqlalchemy.orm import Session
+from app.categories.model.category_model import CategoryModel
+
+
 class CategoryController:
-    pass
+    def get_categories(self, db: Session):
+        return db.query(CategoryModel).all()
+
+    def get_category_by_id(self, db: Session, category_id: int):
+        return db.query(CategoryModel).filter_by(id=category_id).first()
--- app/db/engine.py
+++ app/db/engine.py
@@ -5,7 +5,6 @@ from app.core.config import settings
 engine = create_engine(
     settings.SQLALCHEMY_DATABASE_URI,
     connect_args={"check_same_thread": False},
-    echo=True,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
